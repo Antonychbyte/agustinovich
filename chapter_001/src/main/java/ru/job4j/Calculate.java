4a2b47066ef101bcd9268763ca6a190e32618983
sadko@sadko-N68S3B <anton14024@yandex.ru>
2017-12-14 22:00:01 +0300

    Revert "1.3. Java Doc. Code convention[#34012]"
    This reverts commit 1f28c8d3f5493550413ae816ac69aa74420fa732.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,16 +1,9 @@
 package ru.job4j;
 
-/**
-*class Calculate - решение задачи "1.1. Создание Hello World [#141]"
-*@author Густинович Антон (anton14024@yandex.ru)
-*@since 12.12.17
-*@version 1.0
-*/
 public class Calculate {
-/**
-* Метод выводит на консоль текст "Hello world!"
-*/	
-	public static void main (String [] args){		
+
+	public static void main (String [] args){
+		
 		System.out.println ("Hello world!");
         }
 }
--- chapter_001/src/main/java/ru/job4j/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculate task.
-*
-* @author Густинович Антон (anton14024@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j;
