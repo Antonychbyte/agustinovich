2120c53c87f36d54576184c757db0fcce461b9d4
sadko@sadko-N68S3B <anton14024@yandex.ru>
2017-12-18 23:29:15 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -13,4 +13,13 @@ public class Calculate {
 	public static void main (String [] args){		
 		System.out.println ("Hello world!");
         }
+
+/**
+*Method echo
+*@param name
+*@return Echo plus name
+*/
+         public String echo (String name){
+                return "Echo, Echo, Echo : " + name;
+         }
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*class CalculateTest - решение задачи "1.4. JUnit [#4122]"
+*@author Густинович Антон (anton14024@yandex.ru)
+*@since 15.12.17
+*@version 1.0
+*/
+
+public class CalculateTest{
+
+/**
+*Test echo
+*/
+@Test
+     public void whenTakeNameThenThreeEchoPlusName(){
+
+            String input = "Vasya";
+            String expect = "Echo, Echo, Echo : Vasya";
+
+            Calculate calc = new Calculate();
+            String result =  calc.echo(input);
+
+            assertThat(result, is(expect));
+     }
+}
