1308062d968d017035b8c8be761dee19c1dd3fcc
sadko@sadko-N68S3B <anton14024@yandex.ru>
2018-01-12 00:48:46 +0300

    Revert "3.2. Максимум из двух чисел [#189] if заменил на тернарный"

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,19 +1,13 @@
 package ru.job4j.max;
 
 /**
-* class Max - решение задачи "3.2. Максимум из двух чисел [#189]"
-*@author Густинович Антон (anton14024@yandex.ru)
-*/
+ * class Max - решение задачи "3.2. Максимум из двух чисел [#189]"
+ *@author Густинович Антон (anton14024@yandex.ru)
+ */
 
 public class Max {
 
-       public int max(int first, int second) {
-         int max = first;
-         if (first < second) {
-         max = second;
-         } else if (first == second) {
-           System.out.println("number is equals");
-         }
-         return max;
-       }
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
 }
