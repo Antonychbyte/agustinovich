3c5bdac9e774f5a12ac95cdfa5e8f137271ec9c5
sadko@sadko-N68S3B <anton14024@yandex.ru>
2018-01-29 22:19:43 +0300

    5.4. Двухмерный массив. Таблица умножения.[#33491]

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * class Matrix - решение задачи "5.4. Двухмерный массив. Таблица умножения. [#33491]"
+ *@author Густинович Антон (anton14024@yandex.ru)
+ */
+
+public class Matrix {
+
+    public int[][] multiple(int size) {
+
+        int[][] array = new int[size][size];
+        for (int row = 0; row < size; row++) {
+
+            for (int column = 0; column < size; column++) {
+
+                array[row][column] = (row + 1) * (column + 1);
+            }
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * class MatrixTest - решение задачи "5.4. Двухмерный массив. Таблица умножения.[#33491]"
+ * @author Густинович Антон (anton14024@yandex.ru)
+ */
+
+public class MatrixTest {
+
+    @Test
+    public void whenFourByFourMatrix() {
+
+        Matrix matrix = new Matrix();
+
+        int[][] expect = {
+
+                {1, 2, 3,  4},
+                {2, 4, 6,  8},
+                {3, 6, 9,  12},
+                {4, 8, 12, 16}
+        };
+
+        int[][] result = matrix.multiple(4);
+
+        assertThat(result, is(expect));
+    }
+}
