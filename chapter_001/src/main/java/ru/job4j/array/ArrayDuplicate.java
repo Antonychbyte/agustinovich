5d91f7c9e2fd469235df4ef3c78f00f806178a9a
sadko@sadko-N68S3B <anton14024@yandex.ru>
2018-02-02 16:00:09 +0300

    5.5. Удаление дубликатов в массиве [225]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,42 @@
+package ru.job4j.array;
+
+import static java.util.Arrays.copyOf;
+
+/**
+ * class ArrayDuplicate - решение задачи "5.5.  Удаление дубликатов в массиве. [#225]"
+ *@author Густинович Антон (anton14024@yandex.ru)
+ */
+
+public class ArrayDuplicate {
+
+    public  String[] remove(String[] array) {
+
+        int tail = 0;
+
+        for (int out = 0; out < array.length - tail; out++) {
+
+            for (int in = out+1; in < array.length - tail; in++) {
+
+                if (array[out].equals(array[in])) {
+
+                    String tmp = array[in];
+
+                    array[in] = array[array.length - 1  - tail];
+
+                    array[array.length - 1 - tail] = tmp;
+
+                    in--;
+
+                    tail++;
+                }
+
+            }
+
+        }
+        return copyOf(array, array.length - tail);
+
+    }
+
+}
+
+
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+/**
+ * class ArrayDuplicate - решение задачи "5.5.  Удаление дубликатов в массиве. [#225]"
+ *@author Густинович Антон (anton14024@yandex.ru)
+ */
+
+public class ArrayDuplicateTest {
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+
+           ArrayDuplicate ad = new ArrayDuplicate();
+
+           String[] arTest = {"1", "1", "1", "2", "2", "2"};
+
+           String[] rsl = ad.remove(arTest);
+
+           String[] expect = {"1", "2"};
+
+           assertThat(rsl, arrayContainingInAnyOrder(expect));
+    }
+}
