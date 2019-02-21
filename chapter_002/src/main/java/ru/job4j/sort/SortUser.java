57078cba2b9221a87a1cce87e48c20742dfac010
sadko@sadko-N68S3B <anton14024@yandex.ru>
2019-02-21 09:16:15 +0300

    2. Сортировка User с использованием Comparator [ #10036], рефакторинг Comparator на анонимных классах

--- chapter_002/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort/SortUser.java
@@ -9,12 +9,25 @@ public class SortUser {
   }
 
   public List<User> sortNameLength(List<User> list) {
-    Collections.sort(list, new CompareNameLength());
-    return new ArrayList<User>(list);
+    Collections.sort(list, new Comparator<User>() {
+      @Override
+      public int compare(User left, User right) {
+        int leftLength = left.getName().length();
+        int rightLength = right.getName().length();
+        return Integer.compare(leftLength, rightLength);
+      }
+    });
+    return new ArrayList<>(list);
   }
 
   public List<User> sortByAllFields(List<User> list) {
-    Collections.sort(list, new CompareAllFields());
+    Collections.sort(list, new Comparator<User>() {
+      @Override
+      public int compare(User left, User right) {
+        int compareByName = left.getName().compareTo(right.getName());
+        return compareByName != 0 ? compareByName : Integer.compare(left.getAge(), right.getAge());
+      }
+    });
     return new ArrayList<>(list);
   }
 }
